945674f95559fd30600452a99428dcde0c557cf3
Eduardo Lima <eduardo.lima@dtidigital.com.br>
2023-07-28 19:06:06 -0300

    Correcting deletion of sprint branch

--- scripts/delete-sprint-branch.sh
+++ scripts/delete-sprint-branch.sh
@@ -1,6 +1,1 @@
 git push origin --delete $SPRINT_BRANCH
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [[ "$CURRENT_BRANCH" == "$SPRINT_BRANCH" ]]; then
-    git checkout main
-fi
-git branch -D $SPRINT_BRANCH
