f015632c275ade459dc1bbcf982a8df76a489449
Eduardo Lima <eduardo.lima@dtidigital.com.br>
2023-08-01 19:34:48 -0300

    Checking out correct branch when merging

--- templates/branch-manipulation/scripts/try-merge.sh
+++ templates/branch-manipulation/scripts/try-merge.sh
@@ -2,6 +2,7 @@ git checkout $DESTINATION_BRANCH
 
 git merge --no-ff --no-commit origin/$ORIGIN_BRANCH
 if [[ "$?" == "1" ]]; then
+    git checkout $ORIGIN_BRANCH
     echo "##vso[task.setvariable variable=mergeHasConflicts;]true"
     echo "##vso[task.logissue type=error]There are conflicts between origin $ORIGIN_BRANCH and destination $DESTINATION_BRANCH. Automatic merge is aborted."
     exit 1
@@ -9,5 +10,7 @@ fi
 
 git commit -m "Merge branch 'origin/$ORIGIN_BRANCH' into '$DESTINATION_BRANCH'"
 git push
+
+git checkout $ORIGIN_BRANCH
 
 echo "##vso[task.setvariable variable=mergeHasConflicts;]false"
