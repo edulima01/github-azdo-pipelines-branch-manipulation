7a89826a04f0451744408ad63e1e9ac45ac411db
Eduardo Lima <eduardo.lima@dtidigital.com.br>
2023-08-01 19:16:02 -0300

    Correcting branch creation

--- templates/branch-manipulation/scripts/merge-main/check-release-finished.sh
+++ templates/branch-manipulation/scripts/merge-main/check-release-finished.sh
@@ -1,13 +1,12 @@
 if [[ ! "$MERGE_MESSAGE" =~ ^Merge[[:space:]]branch[[:space:]]\'origin/release/[[:digit:]]{2}-$SQUAD_NAME\'[[:space:]]into[[:space:]]\'main\'$ ]]; then
     exit 0
 fi
+echo "##vso[task.setvariable variable=isMergeNecessary;]false"
 
 CURRENT_BRANCH_NAME=$(echo $MERGE_MESSAGE | awk -F"'" '{print $2}')
 CURRENT_BRANCH_NAME=${CURRENT_BRANCH_NAME:7}
 
 CURRENT_BRANCH_NAME=${CURRENT_BRANCH_NAME#release/}
-echo $CURRENT_BRANCH_NAME
-
 
 CURRENT_SPRINT=${CURRENT_BRANCH_NAME:0:2}
 CURRENT_SQUAD=${CURRENT_BRANCH_NAME:3}
@@ -16,3 +15,5 @@ printf -v PADDED_SPRINT "%02d" $CURRENT_SPRINT
 CREATE_BRANCH_NAME="$SPRINT_BRANCH_PREFIX$PADDED_SPRINT-$CURRENT_SQUAD"
 
 source ./templates/branch-manipulation/scripts/create-branch.sh
+
+git checkout main
