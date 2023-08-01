277d1d9820c013d17d4aea6c7b3bbc07fe87c82d
Eduardo Lima <eduardo.lima@dtidigital.com.br>
2023-08-01 12:01:55 -0300

    Creating merge main to sprints pipeline and creating next sprint branch if necessary

--- templates/branch-manipulation/scripts/finish-release/merge-release-branch-to-main.sh
+++ templates/branch-manipulation/scripts/finish-release/merge-release-branch-to-main.sh
@@ -2,5 +2,6 @@ RELEASE_BRANCH_FULL_NAME=$RELEASE_BRANCH_PREFIX$RELEASE_BRANCH
 
 git checkout main
 
-git merge origin/$RELEASE_BRANCH_FULL_NAME --no-ff
+git merge origin/$RELEASE_BRANCH_FULL_NAME --no-ff --no-commit
+git commit -m "Merge branch 'origin/$RELEASE_BRANCH_FULL_NAME' into 'main'"
 git push
