035498c69fa35cbe306fb5b79f162a774713538d
Eduardo Lima <eduardo.lima@dtidigital.com.br>
2023-08-02 18:35:57 -0300

    Creating start incident pipeline

--- templates/branch-manipulation/scripts/prepare/validate-correct-branch.sh
+++ templates/branch-manipulation/scripts/prepare/validate-correct-branch.sh
@@ -1,4 +1,13 @@
-if [[ ! "$CURRENT_BRANCH" =~ refs/heads/$EXPECTED_BRANCH_PREFIX.* ]]; then
-    echo "##vso[task.logissue type=error]Current branch (${CURRENT_BRANCH:11}) does not have expected prefix ($EXPECTED_BRANCH_PREFIX)."
+IS_TAG=$(echo $IS_TAG | tr '[[:upper:]]' '[[:lower:]]')
+
+BRANCH_TYPE="branch"
+REFS_PREFIX="refs/heads/"
+if [[ "$IS_TAG" = "true" ]]; then
+    BRANCH_TYPE="tag"
+    REFS_PREFIX="refs/tags/"
+fi
+
+if [[ ! "$CURRENT_BRANCH" =~ $REFS_PREFIX$EXPECTED_BRANCH_PREFIX.* ]]; then
+    echo "##vso[task.logissue type=error]Current $BRANCH_TYPE (${CURRENT_BRANCH#${REFS_PREFIX}}) does not have expected prefix ($EXPECTED_BRANCH_PREFIX)."
     exit 1
 fi
